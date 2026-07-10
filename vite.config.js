3aeccee2b54e5032f9bcd01cee733614f67ce3ba
Rahul0001k <jakharrahul525@gmail.com>
2026-07-10 20:15:16 +0530

    fix broken HTML structure and routing

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
-
-// // export default defineConfig({
-// //   plugins: [
-// //     react(),
-// //     ViteImageOptimizer({
-// //       jpeg: { quality: 85, mozjpeg: true },
-// //       jpg:  { quality: 85, mozjpeg: true },
-// //       webp: { quality: 85 },
-// //     }),
-// //   ],
-// //   base: '/'
-// // })
-
-// export default defineConfig({
-//   base: '/',
-//   plugins: [
-//     react(),
-//     ViteImageOptimizer()
-//   ],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
@@ -31,6 +6,10 @@ export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    ViteImageOptimizer()
+    ViteImageOptimizer({
+      jpeg: { quality: 85, mozjpeg: true },
+      jpg:  { quality: 85, mozjpeg: true },
+      webp: { quality: 85 },
+    }),
   ],
 })
